a3b272b2a7e5ef6f5b43c74754389a127dfa8259
Navjashan Singh <navjashansingh@gmail.com>
2018-01-12 10:24:13 -0800

    Calendar Activity linked

--- Masterly/app/src/main/java/com/example/masterly/MainActivity.java
+++ Masterly/app/src/main/java/com/example/masterly/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.masterly;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -76,7 +77,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.notes) {
 
         } else if (id == R.id.calendar) {
-
+            Intent intent = new Intent(this, CalendarActivity.class);
+            startActivity(intent);
         } else if (id == R.id.timer) {
 
         }
